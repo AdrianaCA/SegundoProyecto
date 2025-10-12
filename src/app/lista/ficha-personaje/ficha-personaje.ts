00f13c14283785f920d94a56e1edaff9a0f19d9a
AdrianaCA <cabaleiroa78@gmail.com>
2025-10-12 13:26:48 +0200

    Lista de personajes (2a parte)

--- src/app/lista/ficha-personaje/ficha-personaje.ts
+++ src/app/lista/ficha-personaje/ficha-personaje.ts
@@ -1,14 +1,33 @@
 import { Component, Input } from '@angular/core';
 import { Personaje } from '../../models/Personaje.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-ficha-personaje',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ficha-personaje.html',
   styleUrl: './ficha-personaje.css'
 })
 export class FichaPersonaje {
 
-  @Input() per: Personaje | undefined;
+  @Input() per!: Personaje;
+
+  getFondo(): string {
+    if(this.per.raza == 'Humano') {
+      return '#75A4BA';
+    } else if(this.per.raza == 'Vampiro') {
+      return '#913936';
+    } else if(this.per.raza == 'No humano') {
+      return '#5F4669';
+    } else if(this.per.raza == 'Hombre lobo') {
+      return '#896C53';
+    } else {
+      return '#86A165';
+    }
+  }
+
+  getEstrellas(): string[] {
+    return Array(this.per.poder).fill('⭐');
+  }
 
 }
