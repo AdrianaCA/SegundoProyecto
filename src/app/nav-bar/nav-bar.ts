48bab7be1cbd15d07b9435f4fab1b0a438407547
AdrianaCA <cabaleiroa78@gmail.com>
2025-10-17 17:24:52 +0200

    Porperty binding clase

--- src/app/nav-bar/nav-bar.ts
+++ src/app/nav-bar/nav-bar.ts
@@ -2,15 +2,21 @@ import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
+import {MatMenuModule} from '@angular/material/menu';
+import { CommonModule } from '@angular/common';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [RouterLink ,MatToolbarModule, MatIconModule],
+  imports: [FormsModule , CommonModule , RouterLink ,MatToolbarModule, MatIconModule, MatMenuModule, MatSlideToggleModule],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.css'
 })
 export class NavBar {
 
   pi:string = "pi pi pi";
+
+  esHalloween:boolean = false;
 
 }
