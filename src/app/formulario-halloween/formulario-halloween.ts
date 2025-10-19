e378a5f65cbd286ced6514d6e423785fc364b2b3
AdrianaCA <cabaleiroa78@gmail.com>
2025-10-19 19:48:24 +0200

    Formulario Halloween casa

--- src/app/formulario-halloween/formulario-halloween.ts
+++ src/app/formulario-halloween/formulario-halloween.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-formulario-halloween',
-  imports: [ReactiveFormsModule],
+  imports: [CommonModule , ReactiveFormsModule, MatSelectModule, MatDatepickerModule, MatCheckboxModule],
   templateUrl: './formulario-halloween.html',
   styleUrl: './formulario-halloween.css'
 })
@@ -11,15 +15,29 @@ export class FormularioHalloween {
 
   formulario: FormGroup;
 
+  tipoInvitados: string[] = ['Humano', 'Fantasma', 'Vampiro', 'Bruja'];
+  enviado:boolean = false;
+  mensajeExito: string | null = null;
+
   constructor(private fb: FormBuilder) {
     this.formulario = this.fb.group({
-      nombre: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(10)]],
+      nombre: ['', [Validators.required, Validators.minLength(3)]],
       email: ['', [Validators.required, Validators.email]],
       tipoInvitado: ['', ],
       disfraz: ['', Validators.required],
       fechaLlegada: ['', Validators.required],
       aceptaReglas: ['', Validators.required]
     });
+  }
+
+  submit() {
+    this.enviado = true;
+    if(this.formulario.valid) {
+      this.mensajeExito = '🎃 ¡Bienvenido/a, ' + this.formulario.get('nombre')
+        + '! Tu entrada para la fiesta del castillo ha sido registrada con éxito.';
+    } else {
+      this.mensajeExito = 'Formulario no válido';
+    }
   }
 
 }
